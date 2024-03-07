982f0b9e6fc5bd86dbccdb4e33535cb2094f913a
rhuaannn <rhuanmarques1223@gmail.com>
2024-03-06 21:46:35 -0300

    feat: metodo update,create,delete.

--- intervalo/serializers.py
+++ intervalo/serializers.py
@@ -1,7 +1,10 @@
 from rest_framework import serializers
 from datetime import timedelta
 from intervalo.models import Intervalo
+from professor.models import Professor  # Importe o modelo Professor
 from professor.serializers import ProfessorSerializerGetName
+from datetime import timedelta
+from rest_framework.response import Response
 
 
 class IntervaloGetNameProfessorSerializer(serializers.ModelSerializer):
@@ -12,6 +15,50 @@ class IntervaloGetNameProfessorSerializer(serializers.ModelSerializer):
         fields = ['id', 'entrada', 'almoco', 'retorno_almoco',
                   'saida_expediente', 'descricao', 'professor']
 
+    def create(self, validated_data):
+        nome_professor = validated_data.pop('professor').get('name')
+
+        if not nome_professor:
+            raise serializers.ValidationError(
+                "Nome do professor não fornecido!")
+
+        try:
+            professor = Professor.objects.get(name=nome_professor)
+        except Professor.DoesNotExist:
+            raise serializers.ValidationError(
+                "É necessário criar um professor!")
+
+        intervalo = Intervalo.objects.create(
+            professor=professor, **validated_data)
+
+        return intervalo
+    
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(
+            instance, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+
+        # Verifica se a descrição foi alterada
+        if 'descricao' in serializer.validated_data:
+            almoco = serializer.validated_data.get('almoco', instance.almoco)
+            retorno_almoco = almoco + timedelta(minutes=60)
+            # Verifica se há conflito com outros intervalos
+            if (
+                Intervalo.objects.filter(~Q(pk=instance.pk))
+                .filter(
+                    Q(almoco=almoco) | Q(retorno_almoco=retorno_almoco)
+                )
+                .exists()
+            ):
+                return Response(
+                    {"detail": "Conflito de horários de almoço com outros intervalos."},
+                    status=status.HTTP_400_BAD_REQUEST
+                )
+
+        self.perform_update(serializer)
+        return Response(serializer.data)
+    
     def validate(self, data):
         almoco = data.get('almoco')
         retorno_almoco = data.get('retorno_almoco')
@@ -19,11 +66,13 @@ class IntervaloGetNameProfessorSerializer(serializers.ModelSerializer):
         # Verifica se o horário de retorno do almoço é após o horário de almoço
         if almoco and retorno_almoco and retorno_almoco <= almoco:
             raise serializers.ValidationError(
-                "O horário de retorno do almoço deve ser após o horário de almoço.")
+                "O horário de retorno do almoço deve ser após o horário de almoço."
+            )
 
         # Verifica se a diferença entre o horário de almoço e o horário de retorno do almoço é menor ou igual a 60 minutos
         if almoco and retorno_almoco and retorno_almoco - almoco > timedelta(minutes=60):
             raise serializers.ValidationError(
-                "A diferença entre o horário de almoço e o horário de retorno do almoço não pode exceder 60 minutos.")
+                "A diferença entre o horário de almoço e o horário de retorno do almoço deve ser de no máximo 60 minutos."
+            )
 
         return data
--- intervalo/views.py
+++ intervalo/views.py
@@ -1,6 +1,5 @@
 from rest_framework import generics, status
 from rest_framework.response import Response
-from datetime import timedelta
 from intervalo.models import Intervalo
 from intervalo.serializers import IntervaloGetNameProfessorSerializer
 
@@ -20,25 +19,8 @@ class IntervaloRetriveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Intervalo.objects.all()
     serializer_class = IntervaloGetNameProfessorSerializer
 
-    def update(self, request, *args, **kwargs):
-        instance = self.get_object()
-        serializer = self.get_serializer(instance, data=request.data, partial=True)
+    def put(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-
-        # Verifica se a descrição foi alterada
-        if 'descricao' in serializer.validated_data:
-            almoco = serializer.validated_data.get('almoco', instance.almoco)
-            retorno_almoco = almoco + timedelta(minutes=60)
-            # Verifica se há conflito com outros intervalos
-            if Intervalo.objects.exclude(pk=instance.pk).filter(
-                    almoco=almoco
-            ).exists() or Intervalo.objects.exclude(pk=instance.pk).filter(
-                    retorno_almoco=retorno_almoco
-            ).exists():
-                return Response(
-                    {"detail": "Conflito de horários de almoço com outros intervalos."},
-                    status=status.HTTP_400_BAD_REQUEST
-                )
-
         self.perform_update(serializer)
-        return Response(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
