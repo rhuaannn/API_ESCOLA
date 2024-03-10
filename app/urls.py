a4893a10cec80b77440516c00d3ec6f9da3aef02
rhuaannn <rhuanmarques1223@gmail.com>
2024-03-10 00:51:45 -0300

    feat: pep8

--- app/urls.py
+++ app/urls.py
@@ -18,12 +18,15 @@ urlpatterns = [
 
     path('intervalo/', IntervaloCreateListView.as_view()),
     path('intervalo/<int:pk>', IntervaloRetriveUpdateDestroyView.as_view()),
-    
+
     path('auxiliar/', AuxiliarCreateListView.as_view()),
     path('auxiliar/<int:pk>', AuxiliarRetriveUpdateDestroyView.as_view()),
-    
-    path('authentication/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('authentication/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('authentication/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+
+    path('authentication/token/', TokenObtainPairView.as_view(),
+         name='token_obtain_pair'),
+    path('authentication/token/refresh/',
+         TokenRefreshView.as_view(), name='token_refresh'),
+    path('authentication/token/verify/',
+         TokenVerifyView.as_view(), name='token_verify'),
 
 ]
